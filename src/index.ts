db009eeaf68fb5f5c3785ef468facf218176f2a5
Ebenezr <ebenezarbukosia@gmail.com>
2022-12-28 10:52:53 +0300

    [FIX] fix identation docker compose file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { Request, Response } from "express";
-import routes from "./routes/routes";
+import { Request, Response, NextFunction } from "express";
+const dotenv = require("dotenv");
 const cors = require("cors");
 
 const express = require("express");
@@ -13,9 +13,11 @@ app.use(
 );
 
 app.use(express.json());
-app.use(routes);
 
 const prisma = new PrismaClient();
+
+// load enviroment variables
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
@@ -25,6 +27,96 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(PORT, () =>
   console.log(`REST API server ready at: http://localhost:${PORT}`)
+);
+
+// ROUTES
+// create new doctor
+app.post(
+  "/doctors",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = await prisma.doctor.create({
+        data: { ...req.body },
+      });
+
+      res.json({ success: true, payload: result });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// delete a doctor
+app.delete(
+  `/doctor/:id`,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const song = await prisma.doctor.delete({
+        where: { id: Number(id) },
+      });
+      res.json({
+        success: true,
+        payload: song,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// update doctors
+app.patch(
+  "/doctor/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const doctor = await prisma.doctor.update({
+        where: { id: Number(id) },
+        data: { ...req.body },
+      });
+      res.json({
+        success: true,
+        payload: doctor,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// fetch all doctors
+app.get("/doctors", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const doctors = await prisma.doctor.findMany();
+    res.json({
+      success: true,
+      payload: doctors,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// fetch single doctors
+app.get(
+  "/doctors/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const doctors = await prisma.doctor.findUnique({
+        where: {
+          id: Number(id),
+        },
+      });
+      res.json({
+        success: true,
+        payload: doctors,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 );
 
 // curl -X POST -H "Content-Type: application/json" -d '{
@@ -65,11 +157,11 @@ app.listen(PORT, () =>
 //   });
 // });
 
-// app.use((req: Request, res: Response, next: any) => {
-//   res.status(404);
-//   return res.json({
-//     success: false,
-//     payload: null,
-//     message: `API SAYS: Endpoint not found for path: ${req.path}`,
-//   });
-// });
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404);
+  return res.json({
+    success: false,
+    payload: null,
+    message: `API SAYS: Endpoint not found for path: ${req.path}`,
+  });
+});
