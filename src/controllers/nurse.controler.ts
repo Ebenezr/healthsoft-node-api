fe2bbd7332d79927c8ecbe889c059e8847ffa5fa
Ebenezr <ebenezarbukosia@gmail.com>
2023-01-01 18:49:40 +0300

    [ADD] add nurse controller

---
+++ src/controllers/nurse.controler.ts
@@ -0,0 +1,101 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { PrismaClient } from "@prisma/client";
+import { nextTick } from "process";
+
+const prisma = new PrismaClient();
+const router = Router();
+
+// ROUTES
+// create new nurse
+router.post(
+  "/nurses",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = await prisma.nurse.create({
+        data: { ...req.body },
+      });
+
+      res.json({ success: true, payload: result });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// delete a nurse
+router.delete(
+  `/nurse/:id`,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const song = await prisma.nurse.delete({
+        where: { id: Number(id) },
+      });
+      res.json({
+        success: true,
+        payload: song,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// update nurses
+router.patch(
+  "/nurse/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const nurse = await prisma.nurse.update({
+        where: { id: Number(id) },
+        data: { ...req.body },
+      });
+      res.json({
+        success: true,
+        payload: nurse,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// fetch all nurses
+router.get(
+  "/nurses",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const nurses = await prisma.nurse.findMany();
+      res.json({
+        success: true,
+        payload: nurses,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// fetch single nurses
+router.get(
+  "/nurses/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    try {
+      const nurses = await prisma.nurse.findUnique({
+        where: {
+          id: Number(id),
+        },
+      });
+      res.json({
+        success: true,
+        payload: nurses,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export default router;
